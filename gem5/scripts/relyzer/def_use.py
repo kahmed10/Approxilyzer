5ca1ce941b824aacaf0051b67fc7bc22d5a4db02
Khalique Ahmed <kahmed10@illinois.edu>
2018-06-01 00:23:57 -0500

    added support for multiple bit-widths in def-use

--- gem5/scripts/relyzer/def_use.py
+++ gem5/scripts/relyzer/def_use.py
@@ -1,36 +1,98 @@
 #!/usr/bin/python
 
-import sys
+from inst_database import inst_database
+from inst_database import instruction
+from register import x86_register
+from register import x86_def_register
 
-if len(sys.argv) != 2:
-    print("Usage: python def_use.py [parsed_dis_file]")
-    exit()
+class x86_reg_collection(object):
+    def __init__(self):
+        reg_set = x86_register()
+        self.x86_reg_map = {}
+        self.x86_full_regs = set()
+        for reg in reg_set.reg_alias_map:
+            self.x86_full_regs.add(reg_set.reg_alias_map[reg])
+        for full_reg in self.x86_full_regs:
+            self.x86_reg_map[full_reg] = x86_def_register(full_reg, None)
 
-dis_file = sys.argv[1]
+    def clear_regs(self):
+        for reg in self.x86_reg_map:
+            self.x86_reg_map[reg].update_def(reg, None)
 
-# read and organize disassembly info (1st line is just header)
-dis_info = [i.split() for i in open(dis_file).read().splitlines()[1:]]
+    def __getitem__(self, reg_name):
+        return self.x86_reg_map[reg_name]
+            
 
-pc_list = [item[0] for item in dis_info]
-pc_map = { item[0]:None for item in dis_info }
 
-reg_map = {}
+class def_use_pc_database(object):
+    def __init__(self,app_name,inst_db_filename,create_inst_db=False):
+        
+        insts = []
+        if create_inst_db:
+            dis_filename = inst_db_filename
+            inst_db = inst_database(dis_filename)
+            insts = inst_database.insts
+        else:
+            db_file = inst_db_filename
+            # read and organize disassembly info (1st line is just header)
+            db_info = [i for i in open(db_file).read().splitlines()[1:]]
+            insts = [instruction(None,None,i) for i in db_info]
 
-for item in dis_info:
-    pc = item[0]
-    is_ctrl_op = item[2]
-    src_reg = item[3]
-    dest_reg = item[4]
-    if is_ctrl_op == "True":  # reset map once leaving basic block
-        reg_map = {}
-    else:
-        if dest_reg != "None":
-            reg_map[dest_reg]=pc
-        if src_reg in reg_map:
-            pc_map[reg_map[src_reg]] = pc  # record pc of first use
-            reg_map.pop(src_reg, None) 
-for pc in pc_list:
-    def_use_pair = pc
-    if pc_map[pc] is not None:
-        def_use_pair += " %s" % pc_map[pc]
-    print(def_use_pair)
+        pc_list = [inst.pc for inst in insts]
+        pc_def_map = {}
+
+        x86_regs = x86_register()
+
+        x86_active_regs = x86_reg_collection() 
+
+        for inst in insts:
+            pc = inst.pc
+            is_ctrl_op = inst.ctrl_flag
+            src_regs = inst.src_regs + inst.mem_src_regs
+            dest_reg = inst.dest_reg
+            if is_ctrl_op:  # reset map once leaving basic block
+                x86_active_regs.clear_regs()
+            else:
+                for i in range(len(src_regs)):
+                    src_reg = src_regs[i]
+                    alias_reg = x86_regs.reg_alias_map[src_reg]
+                    alias_dest_reg = x86_active_regs[alias_reg]
+                    src_reg_size = x86_regs.reg_size_map[src_reg]
+                    # check for registers such as 'ah'
+                    if src_reg_size == -1:
+                        bit_range_pc = alias_dest_reg.bit_ranges[1]
+                        if bit_range_pc is not None:
+                            pc_def_map[bit_range_pc].update_first_use(1, pc)
+                    else:
+                        for j in range(src_reg_size):
+                            bit_range_pc = alias_dest_reg.bit_ranges[j]
+                            if bit_range_pc is not None:
+                                pc_def_map[bit_range_pc].update_first_use(j, pc)
+                if dest_reg is not None:
+                    pc_def_map[pc]=x86_def_register(dest_reg, pc)
+                    alias_reg = x86_regs.reg_alias_map[dest_reg]
+                    x86_active_regs[alias_reg].update_def(dest_reg, pc)
+
+        self.pc_list = pc_list
+        self.pc_def_map = pc_def_map
+
+    def print_db(self,output_filename):
+        output = open(output_filename, "w")
+        output.write("pc reg 0-8 8-16 16-32 32-64\n")
+        for pc in self.pc_list:
+            if pc in self.pc_def_map:
+                output.write('%s\n' % self.pc_def_map[pc])
+        output.close()
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Test Usage: python def_use.py [app]")
+        exit()
+
+    app_name = sys.argv[1]
+    inst_db_filename = app_name + "_parsed.txt"
+    def_use_db = def_use_pc_database(app_name,inst_db_filename)
+    output_file = "%s_def_use.txt" % app_name
+    def_use_db.print_db(output_file)

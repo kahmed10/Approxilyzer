555f64d89afb74ce9d90119f971c91eba81b5b03
Khalique Ahmed <kahmed10@illinois.edu>
2018-09-10 09:56:30 -0500

    updated standard run_inj script

--- gem5/scripts/injections/run_injection_x86.sh
+++ gem5/scripts/injections/run_injection_x86.sh
@@ -1,7 +1,7 @@
 #/!bin/bash
 
 if [ $# -ne 6 ]; then
-    echo "Usage: ./run_injection_x86.sh [app_name] [fi_args] [app_ckpt_num] [app_output (in m5out)] [disk_image (in dist/m5/system/disks)] [id]"
+    echo "Usage: ./run_injection_x86_veena.sh [app_name] [fi_args] [app_ckpt_num] [app_output (in m5out)] [disk_image (in dist/m5/system/disks)] [id]"
     exit 1
 fi
 
@@ -18,9 +18,10 @@ CHKPT_DIR=$APPROXGEM5/workloads/x86/checkpoint
 GEM5_DIR=$APPROXGEM5//gem5
 SCRIPTS_DIR=$GEM5_DIR/scripts/injections
 DISK_DIR=$APPROXGEM5/dist/m5/system/disks
-TMP_DIR=/scratch/kahmed10/m5out_$id
+TMP_DIR=/scratch/$LOGNAME/m5out_$id
 OUT_DIR=$GEM5_DIR/outputs/x86
 out_file=$OUT_DIR/$app_name-${out_id}.output
+kernel=$APPROXGEM5/dist/m5/system/binaries/vmlinux-4.9.113
 
 if [ -d $TMP_DIR ]; then  # as a sanity check for now
     rm -rf $TMP_DIR
@@ -37,15 +38,35 @@ cleanup () {
 touch $out_file
 
 app_faulty_output="output.txt"
-if [ "$app_name" == "sobel" ]; then
+if [[ "$app_name" = *"sobel"* ]] || [[ "$app_name" = *"kmeans"* ]]; then
     app_faulty_output="output.pgm"
 fi
+
+if [[ "$app_name" = *"jpeg"* ]]; then
+    app_faulty_output="output.jpg"
+fi
+
 $GEM5_DIR/build/X86/gem5.fast --outdir=$TMP_DIR \
     $GEM5_DIR/configs/example/fs_fi.py --fi=${fi_args} \
-    --disk-image=$DISK_DIR/$disk_image -r $ckpt &> $TMP_DIR/temp_${id}.txt
+    --disk-image=$DISK_DIR/$disk_image \
+    --kernel=$kernel -r $ckpt &> $TMP_DIR/temp_${id}.txt
 
 if grep -q "Timeout" $TMP_DIR/temp_${id}.txt; then 
     echo ${fi_args}"::Detected:Timeout" >> $out_file
+    cleanup
+    exit 0
+fi
+
+if grep -q "segfault" $TMP_DIR/system.pc.com_1.terminal; then
+    echo ${fi_args}"::Detected:segfault" >> $out_file
+    cleanup
+    exit 0
+elif grep -q "Segmentation fault" $TMP_DIR/system.pc.com_1.terminal; then
+    echo ${fi_args}"::Detected:segfault" >> $out_file
+    cleanup
+    exit 0
+elif grep -q "error" $TMP_DIR/system.pc.com_1.terminal; then
+    echo ${fi_args}"::Detected:error" >> $out_file
     cleanup
     exit 0
 fi
@@ -58,6 +79,18 @@ if [ -f "$TMP_DIR/${app_faulty_output}" ]; then
     fi
     if cmp -s $TMP_DIR/$app_output $TMP_DIR/${app_faulty_output}; then
         echo ${fi_args}"::Masked" >> $out_file
+    elif grep -i -q "run-time error" $TMP_DIR/${app_faulty_output}; then
+        echo ${fi_args}"::Detected:run-time error" >> $out_file
+        cleanup
+        exit 0
+    elif grep -q "Segmentation fault" $TMP_DIR/system.pc.com_1.terminal; then
+        echo ${fi_args}"::Detected:segfault" >> $out_file
+        cleanup
+        exit 0
+    elif grep -q "Aborted" $TMP_DIR/system.pc.com_1.terminal; then
+        echo ${fi_args}"::Detected:aborted" >> $out_file
+        cleanup
+        exit 0
 
     else
         result=`perl $SCRIPTS_DIR/detailed_app_level_analysis.pl $app_name $TMP_DIR/${app_faulty_output} $id`
@@ -65,14 +98,7 @@ if [ -f "$TMP_DIR/${app_faulty_output}" ]; then
 
     fi
 else
-    if grep -q "segfault" $TMP_DIR/system.pc.com_1.terminal; then
-        echo ${fi_args}"::Detected:segfault" >> $out_file
-    elif grep -q "error" $TMP_DIR/system.pc.com_1.terminal; then
-        echo ${fi_args}"::Detected:error" >> $out_file
-    else
-        echo ${fi_args}"::Detected:missing_file" >> $out_file
-
-    fi
+    echo ${fi_args}"::Detected:missing_file" >> $out_file
 fi
 
 cleanup
